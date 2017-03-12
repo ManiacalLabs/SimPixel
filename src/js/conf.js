4ed03039ac05e2e5d7ecafff879e7571e790359c
mwcz <mwc@clayto.com>
2017-03-12 12:17:29 -0400

    re-enable change host button

--- src/js/conf.js
+++ src/js/conf.js
@@ -2,13 +2,13 @@
 // just a single button for changing the BiblioPixel host.
 
 function addHostButton(onclick) {
-    // const link = document.createElement('a');
-    // link.innerHTML = 'Change Host';
-    // link.href = '#';
-    // link.addEventListener('click', evt => {
-    //     evt.preventDefault();
-    //     onclick();
-    // });
-    // link.classList.add('conf');
-    // document.body.appendChild(link);
+    const link = document.createElement('a');
+    link.innerHTML = 'Change Host';
+    link.href = '#';
+    link.addEventListener('click', evt => {
+        evt.preventDefault();
+        onclick();
+    });
+    link.classList.add('conf');
+    document.body.appendChild(link);
 }
